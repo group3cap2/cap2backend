89f44c67d7baf8321558942ae769089894efbb28
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-11-11 16:22:47 +0300

    get media

--- routers/routes/media.js
+++ routers/routes/media.js
@@ -1,9 +1,37 @@
 const express = require("express");
-const { getAllMedia } = require("./../controllers/media");
+const {
+  getAllMedia,
+  getAllMovie,
+  getAllPodcast,
+  getAllMusic,
+  getAllMusicVideo,
+  getAllAudiobook,
+  getAllShortFilm,
+  getAllTvShow,
+  getAllSoftware,
+  getAllEbook,
+} = require("./../controllers/media");
 
 const media = express.Router();
 
 media.get("/", getAllMedia);
 
+media.get("/movie", getAllMovie);
+
+media.get("/podcast", getAllPodcast);
+
+media.get("/music", getAllMusic);
+
+media.get("/musicVideo", getAllMusicVideo);
+
+media.get("/audiobook", getAllAudiobook);
+
+media.get("/shortFilm", getAllShortFilm);
+
+media.get("/tvShow", getAllTvShow);
+
+media.get("/software", getAllSoftware);
+
+media.get("/ebook", getAllEbook);
 
 module.exports = media;
