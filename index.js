97b478386890ae20645fd26e7e81b99f3a4035d6
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-11-12 23:31:40 +0300

    app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const helmet = require("helmet");
+// const helmet = require("helmet");
 const cors = require("cors");
 
 const app = express();
@@ -24,7 +24,9 @@ app.use("/favorite", favRouter);
 
 //third party middleware
 app.use(morgan("dev"));
-app.use(helmet());
+// app.use(helmet());
+app.use(cors());
+
 
 
 const PORT = process.env.PORT;
