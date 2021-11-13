241cde845cd41387431dbc0d841f94bd4aa01a1c
SaharAlassaf <92248067+SaharAlassaf@users.noreply.github.com>
2021-11-14 02:55:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(helmet());
 app.use(cors());
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server on ${PORT}`);
   });
